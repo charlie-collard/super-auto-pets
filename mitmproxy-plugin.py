44ce2816327a0eabe9595f450df982775f6c4a42
zi <51308183+nousr@users.noreply.github.com>
2021-11-11 10:10:36 -0800

    Fix error 'no attribute HTTPResponse' (#1)
    * Fix error 'no attribute HTTPResponse'
    
    mitmproxy.http.HTTPResponse might've been an old module, but the newest version of the mitmproxy API uses http.Response.make to generate a response.
    
    Also, removed personal name in filepath & replaced with general path variable to make it more general.
    
    * make path variable more explicit
    
    I realized that it wasn't entirely clear what the path should be to. Updated to include a battle variable as well (probably not necessary but ¯\_(ツ)_/¯ )

--- mitmproxy-plugin.py
+++ mitmproxy-plugin.py
@@ -1,19 +1,21 @@
-from mitmproxy import http
 import re
 import json
+from mitmproxy import http
 
+battle_filename = 'generated-battle.json' # name of the file you generated with make_team.py
+path = f'/path/to/your/{battle_filename}' # <---change this!!
 
 def request(flow: http.HTTPFlow) -> None:
     if not (flow.request.method == "GET"):
         return
     if match := re.search("https://api.teamwoodgames.com/api/battle/get/([-a-f0-9]*)", flow.request.pretty_url):
         (battle_id,) = match.groups()
-        with open("/Users/charlie/workspace/super-auto-pets/generated-battle.json") as f:
+        with open(path) as f:
             data = json.load(f)
         data["Id"] = battle_id
-        flow.response = http.HTTPResponse.make(
-            200,  # (optional) status code
-            bytes(json.dumps(data), encoding="utf8"),  # (optional) content
+        flow.response = http.Response.make(
+            200,
+            bytes(json.dumps(data), encoding="utf8"),
             {
                 "Content-Type": "application/json; charset=utf-8",
                 "Access-Control-Allow-Origin": "*"
