ffc8d7591b0d31935557dfe9055b465e58355cce
Charlie Collard <charliecollard0@gmail.com>
2021-12-10 14:27:19 +0000

    Update api url

--- mitmproxy-plugin.py
+++ mitmproxy-plugin.py
@@ -3,14 +3,13 @@ import json
 from mitmproxy import http
 
 battle_filename = 'generated-battle.json' # name of the file you generated with make_team.py
-path = f'/path/to/your/{battle_filename}' # <---change this!!
 
 def request(flow: http.HTTPFlow) -> None:
     if not (flow.request.method == "GET"):
         return
-    if match := re.search("https://api.teamwoodgames.com/api/battle/get/([-a-f0-9]*)", flow.request.pretty_url):
+    if match := re.search("https://api.teamwoodgames.com/v14/api/battle/get/([-a-f0-9]*)", flow.request.pretty_url):
         (battle_id,) = match.groups()
-        with open(path) as f:
+        with open(battle_filename) as f:
             data = json.load(f)
         data["Id"] = battle_id
         flow.response = http.Response.make(
