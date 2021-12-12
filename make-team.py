82b9cfab8b017b8062673cc4bb8e8b7aae314dec
Charlie Collard <charliecollard0@gmail.com>
2021-12-12 18:13:30 +0000

    Update to new patch, and fix whale

--- make-team.py
+++ make-team.py
@@ -2,56 +2,48 @@ import json
 import re
 
 team_1 = [
-    ("none", "none", 3, 50, 50),
-    ("none", "none", 3, 50, 50),
-    ("none", "none", 3, 50, 50),
-    ("tiger", "none", 3, 1, 1),
-    ("octopus", "cherry", 3, 1, 1),
+    ("whale", "melon", 3, 50, 50),
+    ("whale", "melon", 3, 50, 50),
+    ("whale", "melon", 3, 50, 50),
+    ("whale", "melon", 3, 50, 50),
+    ("whale", "melon", 3, 50, 50),
 ]
 
 team_2 = [
-    ("deer", "none", 2, 50, 15),
-    ("deer", "none", 2, 50, 15),
-    ("whale", "none", 2, 50, 15),
-    ("rooster", "none", 3, 50, 15),
-    ("rooster", "none", 3, 50, 15),
+    ("whale", "melon", 3, 50, 50),
+    ("whale", "melon", 3, 50, 50),
+    ("whale", "melon", 3, 50, 50),
+    ("whale", "melon", 3, 50, 50),
+    ("whale", "melon", 3, 50, 50),
 ]
 
-with open("battle.json") as f:
+with open("data/battle.json") as f:
     data = json.load(f)
 
-with open("animals") as f:
-    animals = f.read().split("\n")
+with open("data/animals.json") as f:
+    animals = json.load(f)
 
-with open("abilities") as f:
-    abilities = f.read().split("\n")
+with open("data/abilities.json") as f:
+    abilities = json.load(f)
 
-with open("perks") as f:
-    perks = f.read().split("\n")
+with open("data/perks.json") as f:
+    perks = json.load(f)
 
-def animal_id(animal):
-    try:
-        return next(a for a in animals if animal.lower() in a.lower()).split(" ")[0]
-    except StopIteration:
-        return None
-
-def ability_id(animal):
-    return next(a for a in abilities if re.search(f"\\b{animal.lower()}\\b", a.lower())).split(" ")[0]
-
-def perk_id(perk):
-    try:
-        return next(p for p in perks if perk.lower() in p.lower()).split(" ")[0]
-    except StopIteration:
-        return None
-
+def transform_ability(ability_id, level):
+    return {
+        "Enum": ability_id,
+        "Level": level,
+        "Native": True,
+        "Duration": 0,
+        "TriggerCount": 1
+    }
 
 for i, (animal, perk, level, attack, health) in enumerate(team_1):
-    if animal_id(animal):
-        data["UserBoard"]["Minions"]["Items"][i]["Enum"] = animal_id(animal)
-        data["UserBoard"]["Minions"]["Items"][i]["Abilities"][0]["Enum"] = ability_id(animal)
-        data["UserBoard"]["Minions"]["Items"][i]["Abilities"][0]["Level"] = level
+    if animal_id := animals.get(animal):
+        data["UserBoard"]["Minions"]["Items"][i]["Enum"] = animal_id
+        data["UserBoard"]["Minions"]["Items"][i]["Abilities"] = [transform_ability(ability_id, level) for ability_id in abilities[animal]]
         data["UserBoard"]["Minions"]["Items"][i]["Level"] = level
-        data["UserBoard"]["Minions"]["Items"][i]["Perk"] = perk_id(perk)
+        data["UserBoard"]["Minions"]["Items"][i]["Perk"] = perks.get(perk)
         data["UserBoard"]["Minions"]["Items"][i]["Attack"]["Permanent"] = attack
         data["UserBoard"]["Minions"]["Items"][i]["Health"]["Permanent"] = health
     else:
@@ -59,12 +51,11 @@ for i, (animal, perk, level, attack, health) in enumerate(team_1):
 
 
 for i, (animal, perk, level, attack, health) in enumerate(team_2):
-    if animal_id(animal):
-        data["OpponentBoard"]["Minions"]["Items"][4-i]["Enum"] = animal_id(animal)
-        data["OpponentBoard"]["Minions"]["Items"][4-i]["Abilities"][0]["Enum"] = ability_id(animal)
-        data["OpponentBoard"]["Minions"]["Items"][4-i]["Abilities"][0]["Level"] = level
+    if animal_id := animals.get(animal):
+        data["OpponentBoard"]["Minions"]["Items"][4-i]["Enum"] = animal_id
+        data["OpponentBoard"]["Minions"]["Items"][4-i]["Abilities"] = [transform_ability(ability_id, level) for ability_id in abilities[animal]]
         data["OpponentBoard"]["Minions"]["Items"][4-i]["Level"] = level
-        data["OpponentBoard"]["Minions"]["Items"][4-i]["Perk"] = perk_id(perk)
+        data["OpponentBoard"]["Minions"]["Items"][4-i]["Perk"] = perks.get(perk)
         data["OpponentBoard"]["Minions"]["Items"][4-i]["Attack"]["Permanent"] = attack
         data["OpponentBoard"]["Minions"]["Items"][4-i]["Health"]["Permanent"] = health
     else:
